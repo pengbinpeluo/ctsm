ae2fc16b25a7776482ccc342eb62a701a0db03ec
pengbinpeluo <112233pengbin@163.com>
2019-11-11 21:52:05 -0700

    change max_phase_name_len to max_str_len_for_phase_def add some parameters for maize

--- src/agsys/ctsm_interface/AgSysParamReader.F90
+++ src/agsys/ctsm_interface/AgSysParamReader.F90
@@ -43,10 +43,18 @@ contains
     allocate(crop_cultivar_params(crop_type_maize)%cultivar_params(1))
 
     associate(cultivar_params => crop_cultivar_params(crop_type_maize)%cultivar_params(1))
+      ! can access params like this:
+      ! cultivar_params%shoot_lag
+      cultivar_params%shoot_lag = 15._r8  ! degree-days
+      cultivar_params%shoot_rate= 0.6_r8  ! degree-days per mm depth
 
-    ! can access params like this:
-    ! cultivar_params%target_tt_from_photoperiod_end_of_juvenile
+      cultivar_params%response_curve_tt%x=[0._r8, 18._r8, 26._r8, 34._r8, 44._r8]
+      cultivar_params%response_curve_tt%y=[0._r8, 10._r8, 18._r8, 26._r8, 0._r8]
+      cultivar_params%response_curve_tt%num_pts=5
 
+      cultivar_params%rc_sw_avail_phenol%x=[0._r8, 0.16_r8]
+      cultivar_params%rc_sw_avail_phenol%y=[0._r8, 1._r8]
+      cultivar_params%rc_sw_avail_phenol%num_pts=2
     end associate
 
   end subroutine ReadParams
@@ -66,6 +74,18 @@ contains
     !-----------------------------------------------------------------------
 
     SHR_ASSERT_FL((size(crop_phases) == crop_type_maxval), sourcefile, __LINE__)
+    crop_phases(crop_type_maize)%stage_name=[character(len=max_str_len_for_phase_def) :: &
+                                              'sowing', 'germination', 'emergence', 'end_of_juvenile', &
+                                              'floral_initiation', 'flag_leaf', 'flowering', &
+                                              'start_grain_fill', 'end_grain_fill', 'maturity', 'harvest_ripe', 'end_crop']
+    crop_pahses(crop_type_maize)%phase_name=[character(len=max_str_len_for_phase_def) :: &
+                                              'germinating', 'emerging', 'juvenile', &
+                                              'photosen_sitive', 'leaf_apperance', 'flag_leaf_to_flowering', &
+                                              'flowering_to_grain_filling', 'grain_filling', 'maturing', 'maturity_to_harvest_ripe', 'ready_for_harvesting']
+    crop_phases(crop_type_maize)%phase_type=[character(len=max_str_len_for_phase_def) :: &
+                                              'germinating', 'emerging', 'generic', &
+                                              'generic', 'leafapperance', 'generic', &
+                                              'generic', 'generic', 'generic', 'generic', 'generic']
 
   end subroutine ReadPhases
 
--- src/agsys/science/AgSysPhases.F90
+++ src/agsys/science/AgSysPhases.F90
@@ -29,7 +29,7 @@ module AgSysPhases
   integer, parameter, public :: composite_phase_type_emerge_to_end_of_juvenile = 2
   integer, parameter, public :: composite_phase_type_maxval = 2
 
-  integer, parameter, public :: max_phase_name_len = 64  ! maximum number of characters in names of phases / stages
+  integer, parameter, public :: max_str_len_for_phase_def = 64  ! maximum number of characters in names of phases / stages / phase types
 
   ! !PUBLIC TYPES:
 
@@ -41,7 +41,7 @@ module AgSysPhases
      private
 
      ! Public data members
-     character(len=max_phase_name_len), public              :: name                ! name of this composite phase
+     character(len=max_str_len_for_phase_def), public       :: name                ! name of this composite phase
      integer, public                                        :: num_child_phases    ! number of child phases (arrays are dimensioned to be this large)
      integer, allocatable, public                           :: child_phase_id(:)   ! index of each child phase (index into arrays in agsys_phases_type)
 
@@ -49,7 +49,7 @@ module AgSysPhases
      ! get from child_phase_id combined with the phase_name array in agsys_phases_type; we
      ! may want to keep this for the sake of consistency checking, but we may want to
      ! remove it.
-     character(len=max_phase_name_len), allocatable, public :: child_phase_name(:) ! name of each child phase
+     character(len=max_str_len_for_phase_def), allocatable, public :: child_phase_name(:) ! name of each child phase
   end type composite_phase_type
 
   ! Information on the phases for a given crop
@@ -66,9 +66,9 @@ module AgSysPhases
      ! given phase. For example, if phase_name(3) = 'juvenile', then the name of the
      ! stage that triggers the start of the juvenile phase is given by stage_name(3)
      ! (e.g., 'emergence').
-     character(len=max_phase_name_len), allocatable, public :: stage_name(:) ! name of the stage that occurs at the start of the given phase
-     character(len=max_phase_name_len), allocatable, public :: phase_name(:) ! name of the given phase
-     integer                          , allocatable, public :: phase_type(:) ! each value is one of the above phase_type_* constants
+     character(len=max_str_len_for_phase_def), allocatable, public :: stage_name(:) ! name of the stage that occurs at the start of the given phase
+     character(len=max_str_len_for_phase_def), allocatable, public :: phase_name(:) ! name of the given phase
+     integer                                 , allocatable, public :: phase_type(:) ! each value is one of the above phase_type_* constants
 
      ! Composite phases for this crop
      !
